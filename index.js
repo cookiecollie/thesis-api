d3d5d9153ad7768698eaefc156ec10864a0c8667
luongphucdien <work.luongphucdien@gmail.com>
2023-09-22 01:09:41 +0700

    feat: Add sign up and sign in methods

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const axios = require("axios")
+const { auth } = require("./firebase")
+const { signInWithEmailAndPassword, createUserWithEmailAndPassword, fetchSignInMethodsForEmail } = require("firebase/auth")
 require("dotenv").config()
 
 const app = express()
@@ -19,8 +21,38 @@ app.get("/", (req, res) => {
     res.json({result: res.statusCode, message: "Success!"})
 })
 
-app.post("/api/user", (req, res) => {
-    res.json({code: res.statusCode, data: req.body})
+app.post("/api/users", async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const user = userCredential.user
+        res.json({code: res.statusCode, user: user})
+    }).catch((error) => {
+        res.json({errorCode: error.code, message: error.message})
+    })
+})
+
+app.post("/api/user/:username", async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        const user = userCredential.user
+        res.json({code: res.statusCode, user: user})
+    }).catch((error) => {
+        res.json({errorCode: error.code, message: error.message})
+    })
+})
+
+app.get("/api/users/:email", async (req, res) => {
+    const email = req.params.email
+
+    await fetchSignInMethodsForEmail(auth, email).then((fetchedEmails) => {
+        res.json({code: axios.HttpStatusCode.Ok, fetchedEmails: fetchedEmails})
+    }).catch((error) => {
+        res.json({errorCode: error.code, message: error.message})
+    })
 })
 
 app.listen(process.env.PORT)
